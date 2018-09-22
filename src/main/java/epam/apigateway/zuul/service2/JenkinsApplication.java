a8b44807e75bb92df2623fffc072d9c817506356
Boppudi, Balasubrahmanyam <Balasubrahmanyam.Boppudi@mastercard.com>
2018-09-22 22:34:25 +0530

    Jenkins practice

---
+++ src/main/java/epam/apigateway/zuul/service2/JenkinsApplication.java
@@ -0,0 +1,12 @@
+package epam.apigateway.zuul.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+}
